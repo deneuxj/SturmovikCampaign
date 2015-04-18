d3112b325309bd3c2e332581ad1676f739e6da38
johann deneux <johann.deneux@gmail.com>
2015-04-18 14:57:15 +0200

    Added console app project to debug code.

---
+++ TestDataProvider/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open SturmovikMission.DataProvider
+
+[<EntryPoint>]
+let main argv =
+
+    let dir = @"C:\users\johann\documents\visual studio 2013\projects\sturmovikmission"
+    let conquest = @"data\Conquest\StalingradConquest.Mission"
+    let path = System.IO.Path.Combine(dir, conquest)
+
+    AutoSchema.getTopTypes (Parsing.Stream.FromFile(path)) |> ignore
+    0
+ 
