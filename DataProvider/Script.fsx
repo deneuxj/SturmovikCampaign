2259dd84fab73bd349ebb6f15897e6dad97d5dd2
johann deneux <johann.deneux@gmail.com>
2015-06-08 19:36:26 +0200

    Removed test code from script in type provider project. Was causing DLL locks, preventing compilations.

--- DataProvider/Script.fsx
+++ DataProvider/Script.fsx
@@ -1,9 +1,2 @@
 ﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
-#load "scripts/load-references.fsx"
-
-#r "bin/Debug/DataProvider.dll"
-
-open SturmovikMission
-
-type T = Provider< @"C:\users\johann\documents\visual studio 2013\projects\sturmovikmission\data\Conquest\StalingradConquest.Mission">
