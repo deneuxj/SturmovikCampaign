46b0094e89c8137d6eddd547c9858f1d3aa893e3
johann deneux <johann.deneux@gmail.com>
2015-06-28 17:25:05 +0200

    Moved cached function to its own module.

---
+++ DataProvider/Cached.fs
@@ -0,0 +1,17 @@
+﻿module SturmovikMission.DataProvider.Cached
+
+open System.Collections.Generic
+
+/// <summary>
+/// Cache calls to a function and its results.
+/// </summary>
+/// <param name="cache">Dictionary which caches results.</param>
+/// <param name="f">Function to cache</param>
+/// <param name="x">Parameter to pass to the function</param>
+let cached (cache : IDictionary<'a, 'b>) f x =
+    match cache.TryGetValue(x) with
+    | true, y -> y
+    | false, _ ->
+        let y = f x
+        cache.Add(x, y)
+        y
