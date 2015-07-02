fdecae3ec0c8d6c15e1a7157f6a7f4cd3afe63a0
johann deneux <johann.deneux@gmail.com>
2015-07-02 22:16:31 +0200

    Thread-safe caching. Very naively implemented, the emphasis is on safety, not performance.

--- DataProvider/Cached.fs
+++ DataProvider/Cached.fs
@@ -25,10 +25,12 @@ open System.Collections.Generic
 /// <param name="cache">Dictionary which caches results.</param>
 /// <param name="f">Function to cache</param>
 /// <param name="x">Parameter to pass to the function</param>
-let cached (cache : IDictionary<'a, 'b>) f x =
-    match cache.TryGetValue(x) with
-    | true, y -> y
-    | false, _ ->
-        let y = f x
-        cache.Add(x, y)
-        y
+let cached (cache : IDictionary<'a, 'b>) f x = 
+    lock cache (fun () -> 
+        match cache.TryGetValue(x) with
+        | true, y -> y
+        | false, _ -> 
+            let y = f x
+            cache.Add(x, y)
+            y)
+
