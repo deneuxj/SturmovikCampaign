5f7aef4a9903145cdb0456544374d22a431dcb7f
johann deneux <johann.deneux@gmail.com>
2015-10-10 19:45:33 +0200

    Added assembly info.

---
+++ CreateTankColumn/AssemblyInfo.fs
@@ -0,0 +1,39 @@
+﻿module AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("SturmovikMission.CreateTankColumn")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Johann Deneux")>]
+[<assembly: AssemblyProduct("CreateTankColumn")>]
+[<assembly: AssemblyCopyright("Copyright (C) Johann Deneux 2015")>]
+[<assembly: AssemblyTrademark("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[<assembly: Guid("fbd3c25e-2afa-4c67-8be5-0575da04e77b")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+
+()
