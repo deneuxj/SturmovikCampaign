c9ed385840a1c2b69d3db7d1187266bc6da673a7
johann deneux <johann.deneux@gmail.com>
2016-01-25 10:43:34 +0100

    Updated a few nuget deps. Moved test code to separate project, removes lots of dependencies from the type provider.

--- DataProvider/Scripts/load-references.fsx
+++ DataProvider/Scripts/load-references.fsx
@@ -1,8 +1,5 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #I __SOURCE_DIRECTORY__
-#r @"System.Core.dll"
-#r @"System.dll"
-#r @"System.Numerics.dll"
-#r @"..\..\packages\FsCheck.2.0.5\lib\net45\FsCheck.dll"
-#r @"..\..\packages\FsCheck.Xunit.2.0.5\lib\net45\FsCheck.Xunit.dll"
-#r @"..\..\packages\xunit.1.9.2\lib\net20\xunit.dll"
+#r "System.Core.dll"
+#r "System.dll"
+#r "System.Numerics.dll"
