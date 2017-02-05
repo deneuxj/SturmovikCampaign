3de5d20c6d0d9bc3591af41a4983f81aac38648a
Johann Deneux <johann.deneux@gmail.com>
2017-02-05 19:16:40 +0100

    Testing of Area.ExtractAreas

--- Campaign/Script.fsx
+++ Campaign/Script.fsx
@@ -1,8 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "Library1.fs"
+#load "Scripts/load-project-debug.fsx"
 open Campaign
+open SturmovikMission.DataProvider.Parsing
+
+let s = Stream.FromFile @"C:\Users\johann\Documents\SturmovikMission-git\data\Blocks\StrategySmall1.mission"
+let data = T.GroupData(s)
+let areas = Area.ExtractAreas(data.ListOfMCU_TR_InfluenceArea)
 
 // Define your library scripting code here
 
---
+++ Campaign/Scripts/load-references-debug.fsx
@@ -0,0 +1,7 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r "../../DataProvider/bin/Debug/DataProvider.dll"
+#r "System.Core.dll"
+#r "System.dll"
+#r "System.Numerics.dll"
+#r "../../Blocks/packages/System.Numerics.Vectors.4.3.0/lib/portable-net45+win8+wp8+wpa81/System.Numerics.Vectors.dll"
