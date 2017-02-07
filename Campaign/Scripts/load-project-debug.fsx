fe543845ef19466d7d15f2bd355824aa471d8bbf
Johann Deneux <johann.deneux@gmail.com>
2017-02-07 21:31:03 +0100

    Move Vector utility code to own file

--- Campaign/Scripts/load-project-debug.fsx
+++ Campaign/Scripts/load-project-debug.fsx
@@ -2,4 +2,5 @@
 #I __SOURCE_DIRECTORY__
 #load "load-references-debug.fsx"
 #load "../AssemblyInfo.fs"
+      "../Vector.fs"
       "../WorldDescription.fs"
--- Campaign/WorldDescription.fs
+++ Campaign/WorldDescription.fs
@@ -4,65 +4,7 @@ open System.Numerics
 
 type T = SturmovikMissionTypes.Provider<"C:\Users\johann\Documents\SturmovikMission-git\data\Sample.mission", "C:\Users\johann\Documents\SturmovikMission-git\data\Blocks\StrategySmall1.Mission">
 
-module Vectors =
-    type Vector2
-    with
-        static member inline FromPos(pos : ^T) =
-            let x = (^T : (member XPos : T.Float) pos).Value
-            let y = (^T : (member ZPos : T.Float) pos).Value
-            Vector2(float32 x, float32 y)
-
-        static member FromPair((x, y) : float * float) =
-            Vector2(float32 x, float32 y)
-
-        static member FromPair(p : T.FloatPair) =
-            Vector2.FromPair(p.Value)
-
-        static member inline FromYOri(ori : ^T) =
-            let angle = (^T : (member YOri : T.Float) ori).Value
-            let alpha = System.Math.PI * angle / 180.0
-            Vector2(float32 <| cos alpha, float32 <| sin alpha)
-
-        static member Cross(u : Vector2, v : Vector2) =
-            u.X * v.Y - u.Y * v.X
-
-        member this.IsInConvexPolygon(poly : Vector2 list) =
-            match poly with
-            | [] -> false
-            | first :: _ ->
-                let cycled = poly @ [first]
-                cycled
-                |> List.pairwise
-                |> List.forall(fun (v1, v2) ->
-                    let v = v2 - v1
-                    let w = this - v1
-                    let c = Vector2.Cross(v, w)
-                    c >= 0.0f
-                )
-
-        member this.DistanceFromSegment(v1 : Vector2, v2 : Vector2) =
-            let w = v2 - v1
-            let wl = w.Length()
-            let v = this - v1
-            if wl <= 1000.0f * System.Single.Epsilon then
-                v.Length()
-            else
-                let w = w / wl
-                let dot = Vector2.Dot(v, w)
-                if dot < 0.0f then
-                    v.Length()
-                elif dot > wl then
-                    (this - v2).Length()
-                else
-                    abs(Vector2.Cross(v, w))
-
-        member this.DistanceFromPath(path : Vector2 seq) =
-            path
-            |> Seq.pairwise
-            |> Seq.map (fun (v1, v2) -> this.DistanceFromSegment(v1, v2))
-            |> Seq.min
-
-open Vectors
+open Vector
 
 type CoallitionId = Axis | Allies
 
