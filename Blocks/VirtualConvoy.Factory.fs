0c0b7162176ed3c081224a21f0ccdb910d1a7912
Johann Deneux <johann.deneux@gmail.com>
2017-03-03 19:01:10 +0100

    Add documentation of links in virtual convoys
    Fixed a couple of bugs (mission implementations) in the process.

--- Blocks/VirtualConvoy.Factory.fs
+++ Blocks/VirtualConvoy.Factory.fs
@@ -299,16 +299,20 @@ with
     member this.CreateLinks() =
         let columns =
             [
+                //  Add trucks to columns
                 for convoy, position, truck in this.TruckInConvoy do
                     yield this.TruckInConvoySet.[truck].Entity, this.ConvoySet.[convoy].LeadCarEntity, position
             ]
         let objectLinks =
             [
+                //  Connect lead car to waypoints
                 for wp, convoy in this.ConvoyAtWaypoint do
                     for wp2 in star this.Path wp do
-                        yield this.ActiveWaypointSet.[wp2].Waypoint :> Mcu.McuTrigger, this.ConvoySet.[convoy].LeadCarEntity :> Mcu.McuBase 
+                        yield this.ActiveWaypointSet.[wp2].Waypoint :> Mcu.McuTrigger, this.ConvoySet.[convoy].LeadCarEntity :> Mcu.McuBase
+                //  Check zone connected to its convoy leader
                 for wec, convoy in this.ConvoyOfEnemyClose do
                     yield this.WhileEnemyCloseSet.[wec].Proximity, this.ConvoySet.[convoy].LeadCarEntity :> Mcu.McuBase
+                //  Connect convoy at last active waypoint to invasion waypoints
                 for start in this.InvasionStart do
                     for wp in star this.InvasionPath start do
                         for _, convoy in this.ConvoyAtWaypoint do
@@ -316,6 +320,7 @@ with
             ]
         let targetLinks =
             [
+                //  Trigger movement to next active waypoint
                 for wp, convoy in this.ConvoyAtWaypoint do
                     match tryGet this.Path wp with
                     | Some wp2 ->
@@ -323,23 +328,33 @@ with
                     | None ->
                         ()
                 for wec, convoy in this.ConvoyOfEnemyClose do
+                    //  Activate convoy when enemy close
                     yield this.WhileEnemyCloseSet.[wec].WakeUp, this.ConvoySet.[convoy].ActivateGroup :> Mcu.McuBase
+                    //  Deactivate convoy when enemy gone
                     yield this.WhileEnemyCloseSet.[wec].Sleep, this.ConvoySet.[convoy].DeactivateGroup :> Mcu.McuBase
                 for curr, next in this.Path do
                     let convoy = get this.ConvoyAtWaypoint curr
                     let convoy2 = get this.ConvoyAtWaypoint next
+                    //  When leader damaged, delete copy at following waypoint
                     yield this.ConvoySet.[convoy].LeadCarDamaged, this.ConvoySet.[convoy2].DeleteLeadCar :> Mcu.McuBase
+                    //  Increase destroyed counter when lead car damaged
+                    yield this.ConvoySet.[convoy].LeadCarDamaged, upcast this.Api.Destroyed
+                    //  When leader deleted at some waypoint, delete at following waypoint too.
                     yield this.ConvoySet.[convoy].DeleteLeadCar, this.ConvoySet.[convoy2].DeleteLeadCar :> Mcu.McuBase
                     for _, pos, truck in filter3 this.TruckInConvoy (Some convoy, None, None) do
                         for _, _, truck2 in filter3 this.TruckInConvoy (Some convoy2, Some pos, None) do
+                            //  When truck damaged, delete copy at following waypoint.
                             yield this.TruckInConvoySet.[truck].Damaged, this.TruckInConvoySet.[truck2].Delete :> Mcu.McuBase
+                            //  When truck deleted, delete copy at following waypoint.
                             yield this.TruckInConvoySet.[truck].Delete, this.TruckInConvoySet.[truck2].Delete :> Mcu.McuBase
                 for wp, convoy in this.ConvoyAtWaypoint do
                     let wec = get this.WhileEnemyCloseOfConvoy convoy
                     let wec = this.WhileEnemyCloseSet.[wec]
                     let wpData = this.ActiveWaypointSet.[wp]
+                    //  Start monitoring when waypoint becomes active (except for the last waypoint)
                     if not(this.PathEnd.Contains(wp)) then
                         yield wpData.Activate, wec.StartMonitoring :> Mcu.McuBase
+                    //  Stop monitoring when waypoint becomes inactive
                     yield wpData.Deactivate, wec.StopMonitoring :> Mcu.McuBase
                 for curr, next in this.Path do
                     let curr2 = this.ActiveWaypointSet.[curr]
@@ -347,28 +362,38 @@ with
                     yield curr2.Waypoint :> Mcu.McuTrigger, next2.Waypoint :> Mcu.McuBase
                     for _, _, timer in filter3 this.TimerBetweenWaypoints (Some curr, Some next, None) do
                         let timer = this.TimerSet.[timer]
+                        //  Activate next waypoint when virtual travel time elapsed, and deactivate current waypoint.
                         yield timer.Elapsed, next2.Activate :> Mcu.McuBase
                         yield timer.Elapsed, curr2.Deactivate :> Mcu.McuBase
                         yield curr2.Activate, timer.Start :> Mcu.McuBase
                 for curr, next in this.Path do
+                    //  When active waypoint physically reached, lead to next waypoint.
+                    yield upcast this.ActiveWaypointSet.[curr].Waypoint, upcast this.ActiveWaypointSet.[next].Waypoint
                     let currWec = get this.EnemyCloseAtWaypoint curr
                     let currWec = this.WhileEnemyCloseSet.[currWec]
+                    //  Prevent showing convoys at following waypoints while convoy at current waypoint is shown.
                     for wp in star this.Path next do
                         let afterWec = get this.EnemyCloseAtWaypoint wp
                         let afterWec = this.WhileEnemyCloseSet.[afterWec]
                         yield currWec.WakeUp, afterWec.Deactivate :> Mcu.McuBase
                         yield currWec.Sleep, afterWec.Activate :> Mcu.McuBase
+                //  Activate first waypoint upon API start
                 for wp in this.PathStart do
                     yield this.Api.Start, this.ActiveWaypointSet.[wp].Activate :> Mcu.McuBase
+                //  TODO: remove, not needed.
                 for wp in this.PathEnd do
                     for start in this.InvasionStart do
                         yield this.ActiveWaypointSet.[wp].Waypoint :> Mcu.McuTrigger, this.SimpleWaypointSet.[start] :> Mcu.McuBase
+                // Increase destroyed counter when truck damaged
                 for convoy, _, truck in this.TruckInConvoy do
                     yield this.TruckInConvoySet.[truck].Damaged, this.Api.Destroyed :> Mcu.McuBase
                 for wp in this.PathEnd do
                     let finish = this.ActiveWaypointSet.[wp]
+                    // Notify of arrival when last waypoint reached (physically)
                     yield finish.Waypoint :> Mcu.McuTrigger, this.Api.Arrived :> Mcu.McuBase
+                    // Notify of arrival when last waypoint reached (virtually)
                     yield finish.Activate, this.Api.Arrived :> Mcu.McuBase
+                //  First invasion waypoint triggered when last active waypoint reached (virtually or physically)
                 for border in this.PathEnd do
                     for enter in this.InvasionStart do
                         match tryGet this.ConvoyAtWaypoint border with
@@ -382,29 +407,39 @@ with
                     let api = this.Api
                     match kvp.Key with
                     | TruckAtDestinationInstance(truck) ->
+                        //  Log arrival of truck WHEN LEADER ARRIVES, unless truck destroyed
                         let truck = this.TruckInConvoySet.[truck]
                         yield api.Arrived, upcast kvp.Value.LeaderArrived
                         yield truck.Delete, upcast kvp.Value.Destroyed
                     | LeadCarAtDestinationInstance(convoy) ->
+                        //  Log arrival of leader, unless it has been damaged.
                         let car = this.ConvoySet.[convoy]
                         yield api.Arrived, upcast kvp.Value.LeaderArrived
                         yield car.DeleteLeadCar, upcast kvp.Value.Destroyed
+                //  Stop monitoring at last travel waypoint when convoy arrives (last travel waypoint reached, physically or virtually)
+                //  FIXME: is this necessary? Maybe avoid starting monitoring. Or maybe do not event include monitor for last travel waypoint.
+                //  NOTE: we actually deactivate all WECs when the convoy arrives. OK, but differs from the spec.
                 for wp, convoy in this.ConvoyAtWaypoint do
                     let wec = get this.WhileEnemyCloseOfConvoy convoy
                     let wec = this.WhileEnemyCloseSet.[wec]
                     let api = this.Api
                     yield api.Arrived, wec.StopMonitoring :> Mcu.McuBase
+                //  Each waypoint in invasion path leads to next waypoint
                 for wp1, wp2 in this.InvasionPath do
                     yield this.SimpleWaypointSet.[wp1] :> Mcu.McuTrigger, this.SimpleWaypointSet.[wp2] :> Mcu.McuBase
                 for finish in this.PathEnd do
                     for prev, _ in this.Path do
                         match tryGet this.ConvoyAtWaypoint prev with
                         | Some convoy ->
-                            let wp = this.ActiveWaypointSet.[prev]
+                            let wp = this.ActiveWaypointSet.[finish]
                             let convoy = this.ConvoySet.[convoy]
+                            // Hide non-last convoy when last travel waypoint reached (physically)
                             yield upcast wp.Waypoint, upcast convoy.DeactivateGroup
+                            // Hide non-last convoy when last travel waypoint reached (virtually)
                             yield wp.Activate, upcast convoy.DeactivateGroup
                         | None -> ()
+                    // Show last convoy when last travel waypoint reached (physically or virtually)
+                    // FIXME: do that only when there is an invasion path.
                     match tryGet this.ConvoyAtWaypoint finish with
                     | Some convoy ->
                         let wp = this.ActiveWaypointSet.[finish]
@@ -413,6 +448,7 @@ with
                         yield wp.Activate, upcast convoy.ActivateGroup
                     | None ->
                         ()
+                // Notify of objective capture when last invasion waypoint reached
                 for finish in this.InvasionEnd do
                     let wp = this.SimpleWaypointSet.[finish]
                     yield upcast wp, upcast this.Api.Captured
