340c2c2f8f728c11bd7165e682e9ed0c99210dad
Johann Deneux <johann.deneux@gmail.com>
2017-03-18 14:28:51 +0100

    Add function to build try-get accessors

--- Campaign/Util.fs
+++ Campaign/Util.fs
@@ -14,6 +14,23 @@ let mkGetStuffFast data mkKey =
             |> dict
     fun x -> m.Value.[x]
 
+/// <summary>
+/// Build a dictionary from a sequence of items and expose it as try-get function.
+/// The entire dictionary is computed at once when the first call to the getter function is called.
+/// </summary>
+/// <param name="data">Sequence of items</param>
+/// <param name="mkKey">Function computing the key of each item</param>
+let mkTryGetStuffFast data mkKey =
+    let m =
+        lazy
+            data
+            |> Seq.map (fun x -> mkKey x, x)
+            |> dict
+    fun x ->
+        match m.Value.TryGetValue(x) with
+        | true, y -> Some y
+        | _ -> None
+
 /// Active pattern checking if a string contains a given substring.
 let (|Contains|_|) substring (s : string) =
     if s.Contains(substring) then
