c2dab29b7afd3d197d5be7603d06cead4f9f6635
Johann Deneux <johann.deneux@gmail.com>
2017-08-13 10:32:17 +0200

    Set version to 1.0

--- AssemblyInfo.fs
+++ AssemblyInfo.fs
@@ -4,15 +4,19 @@ open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
+module Constants =
+    [<Literal>]
+    let version = "1.0.0.0"
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [<assembly: AssemblyTitle("Campaign")>]
-[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyDescription("Persistent dynamic campaign for IL-2 Sturmovik: Battle of Stalingrad")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("Campaign")>]
-[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyCopyright("Copyright © Johann Deneux 2017")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
@@ -34,8 +38,8 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion(Constants.version)>]
+[<assembly: AssemblyFileVersion(Constants.version)>]
 
 do
     ()
