9ee4543cd997016d2eb24681b6dd29c8c86a7875
Johann Deneux <johann.deneux@gmail.com>
2017-09-03 17:57:15 +0200

    Local config change

--- Configuration.fsx
+++ Configuration.fsx
@@ -8,12 +8,12 @@ open Campaign.Configuration
 
 let config =
     { Configuration.Default with
-        PlaneSet = PlaneSet.Moscow
+        PlaneSet = PlaneSet.Stalingrad
         StrategyFile = "StalingradSummer.mission"
-        OutputDir = @"C:\Users\johann\Documents\FromServer\AutoMoscow"
-        //OutputDir = @"C:\Users\johann\Documents\AutoMoscow"
-        ServerDataDir = @"C:\Users\johann\Documents\FromServer"
-        //ServerDataDir = @"E:\dserver\data"
+        //OutputDir = @"C:\Users\johann\Documents\FromServer\AutoMoscow"
+        OutputDir = @"C:\Users\johann\Documents\AutoMoscow"
+        //ServerDataDir = @"C:\Users\johann\Documents\FromServer"
+        ServerDataDir = @"E:\dserver\data"
         ServerBinDir = @"E:\dserver\bin"
         ServerSdsFile = @"sds\devserver.sds"
         ScriptPath = @"C:\Users\johann\Documents\SturmovikCampaign\bin\Debug"
