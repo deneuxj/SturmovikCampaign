9c57c9e01978fd45ac0dc143a5d96467355b6b91
Johann Deneux <johann.deneux@gmail.com>
2017-09-03 18:52:33 +0200

    Fix bug preventing spawn of transfer planes

--- PlaneFerry.fs
+++ PlaneFerry.fs
@@ -13,11 +13,10 @@ let generatePlaneTransfer store (world : World) (state : WorldState) (order : Pl
     // Create mission logic
     let spawnPos, spawnOri = sg.GetAirfield(order.Start).AiSpawnPos
     let landPos, landOri = sg.GetAirfield(order.Destination).Runway
-    let flight = FerryFlight.Create(store, spawnPos, landPos, spawnOri, landOri, order.Qty)
+    let flight = FerryFlight.Create(store, spawnPos, landPos, spawnOri, landOri, order.Qty, order.OrderId.Coalition.ToCountry)
     // Plane type and country
     flight.Plane.Script <- order.Plane.ScriptModel.Script
     flight.Plane.Model <- order.Plane.ScriptModel.Model
-    flight.Plane.Country <- order.OrderId.Coalition.ToCountry
     // Connect to mission start
     Mcu.addTargetLink missionStart flight.Start.Index
     // Result
