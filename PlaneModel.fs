4530378d45d4c2dfe94feeb0ac36cda22f03e711
Johann Deneux <johann.deneux@gmail.com>
2017-09-11 14:06:31 +0200

    Fixes to ground attack payloads

--- PlaneModel.fs
+++ PlaneModel.fs
@@ -210,9 +210,9 @@ with
             | PlaneModel.Bf110e -> 1, 2 // "0,1,2,3-MG17-AP-4000 + 4,5-MGFF-APHE-360 + SC250-2 + SC50-4"
             | PlaneModel.Bf110g -> 1, 2 //  "0,1,2,3-MG17-AP-4000 + 4-MG15120-APHE-400 + 5-MG15120-APHE-350 + SC250-2 + SC50-4"
             | PlaneModel.IL2M41 -> 1, 32 // "0,1-SHKAS-AP-1500 + 2,3-SHVAK-APHE-420 + FAB100M-4 + ROS82-8"
-            | PlaneModel.IL2M42 -> 1, 44 // "0,1-SHKAS-AP-1500 + 2,3-SHVAK-APHE-500 + FAB100M-4 + ROS82-8"
+            | PlaneModel.IL2M42 -> 33, 44 // "0,1-SHKAS-AP-1500 + 2,3-SHVAK-APHE-500 + FAB100M-4 + ROS82-8"
             | PlaneModel.IL2M43 -> 33, 41 // "0,1-SHKAS-AP-1500 + 2,3-SHVAK-APHE-500 + FAB100M-4 + ROS82-4"
-            | PlaneModel.Pe2s35 -> 1, 5 // "0-SHKAS-AP-450 + 1-UB-APHE-150 + FAB250SV-4"
+            | PlaneModel.Pe2s35 -> 5, 5 // "0-SHKAS-AP-450 + 1-UB-APHE-150 + FAB250SV-4"
             | PlaneModel.Hs129b2 -> 1, 4 // "0,1-MG17-AP-2000 + 2,3-MG15115-APHE-500 + SC250-1 + SC50-2"
             | _ -> 1, 0
         modmask, payload
@@ -236,6 +236,7 @@ with
     member this.CargoPayload =
         match this with
         | PlaneModel.Ju52 -> 1, 0
+        | PlaneModel.Pe2s35 | PlaneModel.Pe2s87 -> 1, 0
         | _ -> 1, 0
 
     /// The mod mask and payload ID suitable for a given role
