8763ec93698cb381bfbadaadba0091a76a05659e
Johann Deneux <johann.deneux@gmail.com>
2017-09-30 19:27:16 +0200

    Better error message output for error parsing

--- Init.fsx
+++ Init.fsx
@@ -2,10 +2,21 @@
 
 #I "bin/Debug"
 #r "Campaign.dll"
+#r "DataProvider.dll"
 
 #load "Configuration.fsx" 
 
-let startDate = Campaign.Run.Init.createWorld Configuration.config
+open SturmovikMission.DataProvider
+
+let startDate =
+    try
+        Campaign.Run.Init.createWorld Configuration.config
+    with
+    | :? Parsing.ParseError as e ->
+        Parsing.printParseError e
+        |> String.concat "\n"
+        |> printfn "%s"
+        raise e
 Campaign.Run.WeatherComputation.run(Configuration.config, startDate)
 Campaign.Run.Init.createState Configuration.config
 Campaign.Run.OrderDecision.run Configuration.config
