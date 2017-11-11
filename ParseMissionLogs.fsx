4cc4ea9411c609144e8830c4a74ce6ed371cf536
Johann Deneux <johann.deneux@gmail.com>
2017-11-11 10:56:40 +0100

    Add missing steps

--- ParseMissionLogs.fsx
+++ ParseMissionLogs.fsx
@@ -10,5 +10,12 @@
 open Configuration
 
 let missionResults = Campaign.Run.MissionLogParsing.stage1 config
+
+Campaign.Run.MissionLogParsing.backupFiles config
+let date = Campaign.Run.WeatherComputation.getNextDateFromState config
+let weather = Campaign.Run.WeatherComputation.run(config, date)
+
 let newProduction, battleResults, ((oldState, newState) as states) = Campaign.Run.MissionLogParsing.updateState(config, missionResults)
 let axisAAR, alliesAAR = Campaign.Run.MissionLogParsing.buildAfterActionReports(config, oldState, newState, missionResults.TakeOffs, missionResults.Landings, missionResults.StaticDamages @ missionResults.VehicleDamages, newProduction)
+
+Campaign.Run.MissionLogParsing.stage2 config (oldState, newState, axisAAR, alliesAAR, battleResults)
