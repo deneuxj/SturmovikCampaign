fad41bca73a36c16993e593c3299af49ad7be2a1
Johann Deneux <johann.deneux@gmail.com>
2017-11-27 10:40:51 +0100

    Fix a bug: was using roads and seaways instead of railways

--- MinMax.fs
+++ MinMax.fs
@@ -122,7 +122,7 @@ with
                 world.Regions
                 |> List.map (fun region ->
                     region.Neighbours
-                    |> List.filter (fun ngh -> world.Roads @ world.SeaWays |> List.exists (fun road -> road.MatchesEndpoints(region.RegionId, ngh).IsSome))
+                    |> List.filter (fun ngh -> world.Rails |> List.exists (fun road -> road.MatchesEndpoints(region.RegionId, ngh).IsSome))
                     |> List.map indexOfRegion
                     |> Array.ofList)
                 |> List.indexed
