fec940ee32f643645244827b69978c04daf29cc9
Johann Deneux <johann.deneux@gmail.com>
2017-12-29 11:55:13 +0100

    Add main code

--- CampaignControlApp/Program.fs
+++ CampaignControlApp/Program.fs
@@ -1,7 +1,35 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open ApiImpl
+
+type Options =
+    { ConfigFile : string
+      DoReset : bool
+    }
+with
+    static member Create(argv : string[]) =
+        let config =
+            { ConfigFile = "config.yaml"
+              DoReset = false }
+        argv
+        |> Array.fold (fun config arg ->
+            match arg with
+            | "-r" | "--reset" -> { config with DoReset = true }
+            | path -> { config with ConfigFile = path }) config
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    if argv |> Array.exists (fun s -> s = "-h" || s = "--help" || s = "/?") then
+        printfn """
+Usage: CampaignControlApp [-r] <config.yaml>
+
+Start or continue a campaign. <config.yaml> must point to the campaign's
+configuration file. Pass -r to reset the campaign.
+"""
+        2
+    else
+        let options = Options.Create(argv)
+        use scheduler = new Scheduler(options.ConfigFile)
+        scheduler.ContinueOrReset(options.DoReset)
+        printfn "Press a key to exit."
+        System.Console.ReadKey(false) |> ignore
+        scheduler.Cancel()
+        0
