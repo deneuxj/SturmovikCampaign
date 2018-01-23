a7e10a0511b84e78bb1df0835611af7deb19f3a0
Johann Deneux <johann.deneux@gmail.com>
2018-01-23 20:08:41 +0100

    Ferry flights start in the air

--- Blocks/FerryFlight.fs
+++ Blocks/FerryFlight.fs
@@ -40,23 +40,16 @@ with
         let killed = getTriggerByName group T.Blocks.Killed
         let arrived = getTriggerByName group T.Blocks.JustLanded
         let counter = getTriggerByName group T.Blocks.StartCount :?> Mcu.McuCounter
-        let setCountry =
-            let name =
-                match country with
-                | Mcu.CountryValue.Germany -> T.Blocks.SetGerman
-                | Mcu.CountryValue.Russia -> T.Blocks.SetRussian
-                | _ -> failwithf "unknown country value %d" (int country)
-            getTriggerByName group name
         let spawned = getTriggerByName group T.Blocks.Spawned
-        // When spawned -> set country
-        // The aircraft is initially neutral so that it does not trigger the CZ that checks for occupancy of the spawn area.
-        Mcu.addTargetLink spawned setCountry.Index
         // Position of all nodes
         let refPoint = Vector2.FromMcu plane.Pos
         let dPos = startPos - refPoint
         for mcu in group do
             ((Vector2.FromMcu mcu.Pos - refPoint).Rotate(ori) + dPos + refPoint).AssignTo(mcu.Pos)
             mcu.Ori.Y <- mcu.Ori.Y + float ori
+        // Plane properties
+        plane.Pos.Y <- destination.Pos.Y
+        plane.Country <- Some country
         // Position of destination and land order
         destinationPos.AssignTo destination.Pos
         destinationPos.AssignTo cmdLand.Pos
