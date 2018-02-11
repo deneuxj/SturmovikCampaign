c8cee80d4c517e6f921523fac2200881272a7def
Johann Deneux <johann.deneux@gmail.com>
2018-02-11 16:33:53 +0100

    Add logic to block trains when they reach a destroyed bridge

--- Blocks/Train.fs
+++ Blocks/Train.fs
@@ -9,6 +9,9 @@ open SturmovikMission.Blocks.VirtualConvoy.Types
 open SturmovikMission.Blocks.BlocksMissionData
 open SturmovikMission.Blocks.IconDisplay
 open SturmovikMission.Blocks.EventReporting
+open SturmovikMission.Blocks.VirtualConvoy.Factory
+open SturmovikMission.Blocks.Conjunction
+open SturmovikMission.Blocks.Util.String
 
 type Train = {
     Start : Mcu.McuTrigger
@@ -20,7 +23,11 @@ type Train = {
     All : McuUtil.IMcuGroup
 }
 with
-    static member Create(store : NumericalIdentifiers.IdStore, lcStore, pos : Vector2, yori : float32, destinationPos : Vector2, country : Mcu.CountryValue) =
+    static member Create(store : NumericalIdentifiers.IdStore, lcStore, path : PathVertex list, bridges : (PathVertex * Mcu.McuEntity) list, country : Mcu.CountryValue) =
+        if path.IsEmpty then
+            failwith "Cannot create train with empty path"
+        let startV = List.head path
+        let destV = List.last path
         // Instantiate
         let subst = Mcu.substId <| store.GetIdMapper()
         let group = blocksData.GetGroup("Train").CreateMcuList()
@@ -33,6 +40,65 @@ with
         let blocked = getTriggerByName group T.Blocks.BLOCKED
         let destWp = getWaypointByName group T.Blocks.Destination
         let train = getVehicleByName group T.Blocks.Train
+        // Logic to stop and block train when it reaches a destroyed bridge
+        let conds =
+            seq {
+                for v, entity in bridges do
+                    let conj = Conjunction.Create(store, v.Pos + Vector2(0.0f, 100.0f))
+                    // Link bridge destruction to conjA
+                    entity.OnEvents <-
+                        { Mcu.Type = int Mcu.EventTypes.OnKilled
+                          Mcu.TarId = conj.SetA.Index }
+                        :: entity.OnEvents
+                    // Link conj result to block signal
+                    Mcu.addTargetLink conj.AllTrue blocked.Index
+                    // Result
+                    yield v, conj
+            }
+            |> Seq.groupBy fst
+            |> Seq.map (fun (k, vs) -> k, vs |> Seq.map snd |> List.ofSeq)
+            |> dict
+        let getConjOfVertex v =
+            match conds.TryGetValue v with
+            | true, x -> x
+            | false, _ -> []
+        // Intermediate waypoints to destWp
+        let wps =
+            let subst = Mcu.substId <| store.GetIdMapper()
+            [
+                for idx, (v, _) in Seq.indexed(Seq.pairwise path) do
+                    let waypoint = newWaypoint (idx + 1) v.Pos v.Ori v.Radius v.Speed v.Priority
+                    subst waypoint
+                    // Object-link to train
+                    Mcu.addObjectLink waypoint train.LinkTrId
+                    // Target-link to conjunctions, if any
+                    match getConjOfVertex v with
+                    | [] -> ()
+                    | conds ->
+                        waypoint.Radius <- max waypoint.Radius 1000 // Make sure the train has enough time to stop before getting to the destroyed bridge
+                        for conj in conds do
+                            Mcu.addTargetLink waypoint conj.SetB.Index
+                        yield waypoint
+            ]
+        // start -> first intermediate waypoint
+        match wps with
+        | wp1 :: _ ->
+            Mcu.addTargetLink start wp1.Index
+            // remove any link to destWp
+            start.Targets <-
+                start.Targets
+                |> List.filter ((<>) destWp.Index)
+        | [] ->
+            ()
+        // intermediate wp -> next wp
+        for v1, v2 in Seq.pairwise wps do
+            Mcu.addTargetLink v1 v2.Index
+        // Last intermediate wp -> destWp
+        match List.tryLast wps with
+        | Some last ->
+            Mcu.addTargetLink last destWp.Index
+        | None ->
+            ()
         // Override train
         let train2 =
             match country with
@@ -41,18 +107,18 @@ with
             | _ -> vehicles.MkGermanTrainMcu()
         train2.Index <- train.Index
         train2.LinkTrId <- train.LinkTrId
-        train2.Ori.Y <- float yori
+        train2.Ori.Y <- float startV.Ori
         // Position of all nodes
         let refPoint = Vector2(float32 train.Pos.X, float32 train.Pos.Z)
-        let dv = pos - refPoint
+        let dv = startV.Pos - refPoint
         for mcu in group do
             (Vector2.FromMcu(mcu.Pos) + dv).AssignTo(mcu.Pos)
         McuUtil.vecCopy train.Pos train2.Pos
         // Position waypoint
-        destinationPos.AssignTo destWp.Pos
+        destV.Pos.AssignTo destWp.Pos
         // Icons
         let iconPos =
-            0.5f * (pos + destinationPos)
+            0.5f * (startV.Pos + destV.Pos)
         let coalition =
             match country with
             | Mcu.CountryValue.Germany -> Mcu.CoalitionValue.Axis
@@ -74,7 +140,7 @@ with
           IconCover = iconCover
           IconAttack = iconAttack
           All = { new McuUtil.IMcuGroup with
-                      member x.Content = group
+                      member x.Content = (wps |> List.map(fun x -> upcast x)) @ group
                       member x.LcStrings = []
                       member x.SubGroups = [ iconCover.All; iconAttack.All ]
           }
@@ -100,8 +166,12 @@ with
                 yield this.Destroyed.All
             ]
 
-    static member Create(store, lcStore, pos, yori, destinationPos, country, eventName) =
-        let train = Train.Create(store, lcStore, pos, yori, destinationPos, country)
+    static member Create(store, lcStore, path : PathVertex list, bridges, country, eventName) =
+        if path.IsEmpty then
+            failwith "Cannot create train with empty path"
+        let pos = path.Head.Pos
+        let destinationPos = (List.last path).Pos
+        let train = Train.Create(store, lcStore, path, bridges, country)
         let startEventName = sprintf "%s-D-0" eventName
         let started = EventReporting.Create(store, country, pos + Vector2(0.0f, 100.0f), startEventName)
         let arrivedEventName = sprintf "%s-A-0" eventName
