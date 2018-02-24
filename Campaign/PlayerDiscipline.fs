9aec407b4888df74a87ce9ef91bce03e32295955
Johann Deneux <johann.deneux@gmail.com>
2018-02-24 13:56:39 +0100

    License under GPL v3

--- Campaign/PlayerDiscipline.fs
+++ Campaign/PlayerDiscipline.fs
@@ -1,4 +1,20 @@
-﻿module Campaign.PlayerDiscipline
+﻿// SturmovikCampaign, a dynamic persistent campaign for IL-2 Sturmovik: Battle of Stalingrad
+// Copyright (C) 2018 Johann Deneu <johann.deneux@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+module Campaign.PlayerDiscipline
 
 open System
 open Campaign.BasicTypes
