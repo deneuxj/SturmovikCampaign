591e9bc60f5bc499c3f0de9ea3d2eb2b9d6bebbe
Johann Deneux <johann.deneux@gmail.com>
2018-04-08 17:06:14 +0200

    Version 1.16.9.0

--- Campaign/AssemblyInfo.fs
+++ Campaign/AssemblyInfo.fs
@@ -22,7 +22,7 @@ open System.Runtime.InteropServices
 
 module Constants =
     [<Literal>]
-    let version = "1.16.8.0"
+    let version = "1.16.9.0"
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
