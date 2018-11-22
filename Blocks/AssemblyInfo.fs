53a29f4802b968e0c59e0f7b730a0a745a5e6631
Johann Deneux <johann.deneux@gmail.com>
2018-11-22 21:40:25 +0100

    Version 1.11.2.0 of blocks

--- Blocks/AssemblyInfo.fs
+++ Blocks/AssemblyInfo.fs
@@ -6,7 +6,7 @@ open System.Runtime.InteropServices
 
 module Constants =
     [<Literal>]
-    let version = "1.11.1.0"
+    let version = "1.11.2.0"
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
