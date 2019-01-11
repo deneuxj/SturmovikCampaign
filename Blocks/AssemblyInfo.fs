0d689b6b5ded8093559f45db5777f6ec459dea65
Johann Deneux <johann.deneux@gmail.com>
2019-01-11 21:30:20 +0100

    Version 1.14.0.0 of Blocks

--- Blocks/AssemblyInfo.fs
+++ Blocks/AssemblyInfo.fs
@@ -6,7 +6,7 @@ open System.Runtime.InteropServices
 
 module Constants =
     [<Literal>]
-    let version = "1.13.0.0"
+    let version = "1.14.0.0"
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
