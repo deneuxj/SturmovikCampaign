7afe5bb24c447b8294bac44e1e810210d358b1be
Johann Deneux <johann.deneux@gmail.com>
2019-11-09 11:50:28 +0100

    Added new world description type for new air campaign

---
+++ Campaign/Src/NewWorldDescription.fs
@@ -0,0 +1,86 @@
+﻿// SturmovikCampaign, a dynamic persistent campaign for IL-2 Sturmovik: Battle of Stalingrad
+// Copyright (C) 2019 Johann Deneux <johann.deneux@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+module Campaign.NewWorldDescription
+
+open System.Numerics
+open VectorExtension
+
+open SturmovikMission.DataProvider
+open SturmovikMission.Blocks.BlocksMissionData
+open SturmovikMission.Blocks
+open SturmovikMission.Blocks.StaticDefenses.Types
+open SturmovikMission.Blocks.BlocksMissionData.CommonMethods
+open SturmovikMission.DataProvider.Parsing
+open SturmovikMission.Blocks.VirtualConvoy.Types
+open SturmovikMission.Blocks.VirtualConvoy.Factory
+open Util
+
+open Campaign.BasicTypes
+open Campaign.PlaneSet
+open Campaign.WorldDescription
+
+type BuildingProperties = {
+    Model : string
+    Script : string
+    Boundary : Vector2 list
+    SubParts : int list
+}
+
+type BuildingInstance = {
+    Pos : OrientedPosition
+    Properties : BuildingProperties
+}
+
+type Region = {
+    RegionId : RegionId
+    Boundary : Vector2 list
+    Neighbours : RegionId list
+    InitialOwner : CoalitionId option
+    // Production in the region; Typically only the rearmost region should have production.
+    // It represents the entry point into the game for supplies, and cannot be affected.
+    Production : float32<E/H>
+    // Supplies can pass through the region, affected by industry
+    FlowCapacity : float32<E/H>
+    IndustryBuildings : BuildingInstance list
+}
+
+/// A node in the logistics network
+type NetworkNode = {
+    Id : int
+    Pos : Vector2
+    Radius : Vector2
+    // Bridges, train stations... Affect the flow capacity when damaged
+    Facilities : BuildingInstance list
+    FlowCapacity : float32<E/H>
+    Region : RegionId
+    Neighbours : int list 
+}
+
+type Runway = {
+    SpawnPos : OrientedPosition
+    PathToRunway : Vector2 list
+    Start : Vector2
+    End : Vector2
+    Facilities : BuildingInstance list
+}
+
+type Airfield = {
+    AirfieldId : AirfieldId
+    Region : RegionId
+    Runways : Runway list
+    Facilities : StaticGroup list
+}
