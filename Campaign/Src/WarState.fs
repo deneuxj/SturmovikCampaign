4dffa4d21d7d95749dde6e7bad64b29c9e990edb
Johann Deneux <johann.deneux@gmail.com>
2019-12-07 20:24:17 +0100

    Change to TargetType

--- Campaign/Src/WarState.fs
+++ Campaign/Src/WarState.fs
@@ -1,4 +1,20 @@
-﻿namespace Campaign.WarState
+﻿// SturmovikCampaign, a dynamic persistent campaign for IL-2 Sturmovik: Battle of Stalingrad
+// Copyright (C) 2019 Johann Deneux <johann.deneux@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace Campaign.WarState
 
 open System
 open System.Numerics
@@ -9,8 +25,10 @@ open Campaign.WorldDescription
 open Campaign.NewWorldDescription
 
 type TargetType =
-    | Truck | Train | Ship | Battleship | Artillery | Tank | ArmoredCar | Building  | Bridge | ParkedPlane
-    | Air of PlaneType * float32
+    | Truck | Train | Ship | Battleship | Artillery | Tank | ArmoredCar | Bridge
+    | Building of BuildingProperties * int
+    | ParkedPlane of PlaneType
+    | Air of PlaneType
 
 type Target =
     {
