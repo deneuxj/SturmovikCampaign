c66f0b9549e94386b2fc204d9842a76519b4cf7b
Johann Deneux <johann.deneux@gmail.com>
2020-01-03 19:27:46 +0100

    Add commands to change and move ground forces

--- Campaign/Src/WarStateUpdate.fs
+++ Campaign/Src/WarStateUpdate.fs
@@ -38,11 +38,18 @@ type Commands =
     | RemovePlane of AirfieldId * PlaneModelId * Health: float32
     // Add a plane to an airfield
     | AddPlane of AirfieldId * PlaneModelId * Health: float32
+    // Add ground forces to a region
+    | AddGroundForces of RegionId * CoalitionId * Amount: float32<MGF>
+    // Destroy ground forces in a region
+    | DestroyGroundForces of RegionId * CoalitionId * Amount: float32<MGF>
+    // Move ground forces from a region to another
+    | MoveGroundForces of RegionId * RegionId * CoalitionId * Amount: float32<MGF>
 
 /// Interesting data to report from execution of commands
 type Results =
     | UpdatedStorageValue of Instance: BuildingInstanceId * Amount: float32<M^3>
     | UpdatedPlanesAtAirfield of AirfieldId * Map<PlaneModelId, float32>
+    | UpdatedGroundForces of RegionId * CoalitionId * float32<MGF>
 
 module DamageExtension =
     type WarState with
@@ -65,19 +72,38 @@ module DamageExtension =
             this.SetNumPlanes(afid, plane, qty)
             this.GetNumPlanes(afid)
 
+        /// Add or remove forces of a coalition in a region, return the new amount of forces
+        member this.ChangeGroundForces(rid, coalition, delta) =
+            let forces =
+                this.GetGroundForces(coalition, rid) + delta
+                |> max 0.0f<MGF>
+            this.SetGroundForces(coalition, rid, forces)
+            forces
+
     type Commands with
         /// Execute commands on a WarState. Return the result of the command.
         member this.Execute(state : WarState) =
             match this with
             | DamageBuilding(bid, part, dmg) ->
                 let storage = state.ChangeHealth(bid, part, -dmg)
-                UpdatedStorageValue(bid, storage)
+                [ UpdatedStorageValue(bid, storage) ]
             | RepairBuilding(bid, part, heal) ->
                 let storage = state.ChangeHealth(bid, part, heal)
-                UpdatedStorageValue(bid, storage)
+                [ UpdatedStorageValue(bid, storage) ]
             | AddPlane(afid, plane, health) ->
                 let newStatus = state.ChangePlanes(afid, plane, health)
-                UpdatedPlanesAtAirfield(afid, newStatus)
+                [ UpdatedPlanesAtAirfield(afid, newStatus) ]
             | RemovePlane(afid, plane, health) ->
                 let newStatus = state.ChangePlanes(afid, plane, -health)
-                UpdatedPlanesAtAirfield(afid, newStatus)
+                [ UpdatedPlanesAtAirfield(afid, newStatus) ]
+            | AddGroundForces(rid, coalition, amount) ->
+                let newForces = state.ChangeGroundForces(rid, coalition, amount)
+                [ UpdatedGroundForces(rid, coalition, newForces) ]
+            | DestroyGroundForces(rid, coalition, destroyed) ->
+                let newForces = state.ChangeGroundForces(rid, coalition, -destroyed)
+                [ UpdatedGroundForces(rid, coalition, newForces) ]
+            | MoveGroundForces(start, destination, coalition, forces) ->
+                let startForces = state.ChangeGroundForces(start, coalition, forces)
+                let destinationForces = state.ChangeGroundForces(destination, coalition, -forces)
+                [ UpdatedGroundForces(start, coalition, startForces)
+                  UpdatedGroundForces(destination, coalition, destinationForces) ]
