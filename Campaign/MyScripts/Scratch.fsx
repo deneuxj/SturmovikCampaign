d259324ad63e569e17adc669cc8f15a3e23b31e4
Johann Deneux <johann.deneux@gmail.com>
2020-01-26 19:31:52 +0100

    Test extraction of planes from plane database

--- Campaign/MyScripts/Scratch.fsx
+++ Campaign/MyScripts/Scratch.fsx
@@ -1,4 +1,5 @@
 ﻿#I @"..\bin\Debug"
+#I @"C:\Users\johann\Documents\SturmovikCampaign\packages\FSharp.Data.3.3.2\lib\net45"
 
 #r "Campaign.dll"
 #r "System.Numerics.Vectors.dll"
@@ -11,18 +12,17 @@ open System.IO
 open System.Numerics
 open Campaign.BasicTypes
 open Campaign.NewWorldDescription
+open Campaign.WarState
+open Campaign.WarStateUpdate
+open Campaign.Missions
+open Campaign.MissionGenerator
+open Campaign.PlaneModel
 
-let nearestNodes (v : Vector2) (nodes : NetworkNode list) =
-    let withinRadius =
-        nodes
-        |> List.filter (fun node -> (node.Pos - v).LengthSquared() <= 25e6f)
-    withinRadius
-    |> List.sortBy (fun node -> (node.Pos - v).LengthSquared())
-    |> List.truncate 5
+let planeDb =
+    planeDb
+    |> List.map (fun plane -> plane.Id, plane)
+    |> Map.ofList
 
-let dir = @"C:\Users\johann\Documents\ungtp\Maps\graphics\landscape_rheinland_wi\roads"
-let origin = Vector2(30000.0f, 30000.0f)
-let corner = Vector2(354000.0f, 431000.0f)
-let network = NetworkNode.NodesFromIni(Path.Combine(dir, "roadssystem.ini"), Path.Combine(dir, "railroads.ini"), origin, corner, 1.0f<E/H>)
-
-nearestNodes (Vector2(290000.0f, 390000.0f)) network
+let planeSet =
+    Bodenplatte.allPlanesOf Axis @ Bodenplatte.allPlanesOf Allies
+    |> List.choose (fun plane -> planeDb.TryFind plane |> Option.orElseWith (fun () -> printfn "%s missing" (string plane); None))
