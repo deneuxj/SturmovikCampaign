bd4cc281b315372fc93c15d1c376333fa197245c
Johann Deneux <johann.deneux@gmail.com>
2020-02-08 17:34:29 +0100

    Add function to keep advancing while the war is ongoing.
    Also pretty global stats before each step.

--- Campaign/MyScripts/Scratch.fsx
+++ Campaign/MyScripts/Scratch.fsx
@@ -48,19 +48,74 @@ Bodenplatte.initAirfields Allies war
 let mutable step = Bodenplatte.start war
 let random = System.Random(0)
 
-let advance() =
+type WarState with
+    member this.AllIndustry (coalition : CoalitionId) =
+        this.World.Regions.Values
+        |> Seq.filter (fun region -> this.GetOwner region.RegionId = Some coalition)
+        |> Seq.sumBy (fun region -> this.GetRegionBuildingCapacity region.RegionId)
+
+    member this.AllAirfieldsOperationality (coalition : CoalitionId) =
+        this.World.Airfields.Values
+        |> Seq.filter (fun af -> this.GetOwner af.Region = Some coalition)
+        |> Seq.sumBy (fun af ->
+            af.Facilities
+            |> Seq.sumBy this.GetBuildingCapacity)
+
+    member this.AllGroundForces (coalition : CoalitionId) =
+        this.World.Regions.Values
+        |> Seq.sumBy (fun region -> this.GetGroundForces(coalition, region.RegionId))
+
+    member this.AllPlanes (coalition : CoalitionId, planeType : PlaneType) =
+        this.World.Airfields.Values
+        |> Seq.filter (fun af -> this.GetOwner af.Region = Some coalition)
+        |> Seq.sumBy (fun af ->
+            this.GetNumPlanes(af.AirfieldId)
+            |> Map.toSeq
+            |> Seq.sumBy (fun (plane, qty) -> if this.World.PlaneSet.[plane].Kind = planeType then qty else 0.0f)
+        )
+
+    member this.GetSummaryColums() =
+        sprintf "%6s|%10s|%10s|%10s|%10s|%11s|%10s|%11s"
+            "Side" "Industry" "Airfields" "Ground" "Bombers" "Attackers" "Fighters" "Transport"
+
+    member this.GetSummary (coalition : CoalitionId) =
+        sprintf "%6s|%10.0f|%10.0f|%10.0f|%10.0f|%11.0f|%10.0f|%11.0f"
+            (string coalition)
+            (this.AllIndustry coalition)
+            (this.AllAirfieldsOperationality coalition)
+            (this.AllGroundForces coalition)
+            (this.AllPlanes(coalition, PlaneType.Bomber))
+            (this.AllPlanes(coalition, PlaneType.Attacker))
+            (this.AllPlanes(coalition, PlaneType.Fighter))
+            (this.AllPlanes(coalition, PlaneType.Transport))
+
+let advance verbose =
     match step with
     | Stalemate -> printfn "Stalemate"
     | Victory side -> printfn "%s is victorious" (string side)
     | Ongoing data ->
         printfn "New mission: %s" data.Briefing
+        printfn "%s" (war.GetSummaryColums())
+        printfn "%s" (war.GetSummary Axis)
+        printfn "%s" (war.GetSummary Allies)
         let sim = MissionSimulator(random, war, data.Missions, 10.0f<H>)
         let events = sim.DoAll()
         for cmd, descr in events do
-            printfn "Action: %s" descr
+            if verbose then
+                printfn "Action: %s" descr
             cmd
             |> Option.iter (fun cmd ->
                 for result in cmd.Execute(war) do
-                    printfn "Result: %s" (Results.asString war result))
+                    if verbose then
+                        printfn "Result: %s" (Results.asString war result))
         step <- data.Next war
 
+let advanceStar verbose =
+    let rec work() =
+        match step with
+        | Ongoing _ ->
+            advance verbose
+            work()
+        | _ ->
+            ()
+    work()
