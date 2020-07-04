63d75187ab94d13e08c69ae617799cb7ca801c45
Johann Deneux <johann.deneux@gmail.com>
2020-07-04 23:55:58 +0200

    Convert from 0.0-1.0 coordinates to the game's

--- FreeAreas/Program.fs
+++ FreeAreas/Program.fs
@@ -9,14 +9,27 @@ open MBrace.FsPickler
 
 let parseNum x = Single.Parse(x, System.Globalization.CultureInfo.InvariantCulture)
 
-let getPoints (path : string) =
+let mkScaleCoords mapName =
+    // lower and upper corners, using the game's coordinates (X -> north, Y -> east).
+    // Actually, in the game it's Z that goes east, but we skip elevation Y.
+    let ll, ur =
+        match mapName with
+        | "kuban" -> Vector2(35000.0f, 35000.0f), Vector2(323000.0f, 450000.0f)
+        | "moscow" -> Vector2.Zero, Vector2(281000.0f, 281000.0f)
+        | "rheinland" -> Vector2(30000.0f, 30000.0f), Vector2(351000.0f, 431000.0f)
+        | "stalingrad" -> Vector2.Zero, Vector2(230000.0f, 358000.0f)
+        | _ -> failwithf "Unsupported map '%s'" mapName
+    fun (x, y) ->
+        ll + Vector2(y * (ur.X - ll.X), x * (ur.Y - ll.Y))
+
+let getPoints scaleCoords (path : string) =
     seq {
         use file = File.OpenText(path)
         while not file.EndOfStream do
             let line = file.ReadLine()
             let components = line.Split ','
             match components with
-            | [| cx; cy |] -> yield Vector2(parseNum cx, parseNum cy)
+            | [| cx; cy |] -> yield scaleCoords(parseNum cx, parseNum cy)
             | [||] -> ()
             | _ -> failwithf "Ill-formed line '%s'" line
     }
@@ -27,12 +40,12 @@ let mkQuadTree (points : Vector2 seq) =
 [<EntryPoint>]
 let main argv =
     match argv |> List.ofArray with
-    | (path :: _) as paths ->
+    | mapName :: paths ->
         use resultFile = File.Create("free-areas.bin")
         let watch = Stopwatch.StartNew()
         let tree =
             paths
-            |> Seq.collect getPoints
+            |> Seq.collect (getPoints (mkScaleCoords mapName))
             |> mkQuadTree
         printfn "Top node bounds: %A %A" tree.Root.Min tree.Root.Max
         let time = watch.ElapsedMilliseconds
@@ -50,5 +63,5 @@ let main argv =
         printfn "Translation and serialization took %f s" ((float time) / 1000.0)
         0
     | _ ->
-        printfn "Missing path to obstacle list file"
+        printfn "Missing map name and/or point lists"
         1
