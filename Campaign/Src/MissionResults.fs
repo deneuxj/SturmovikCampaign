bc891be6297701ee451dfa7440c09e56ec33e353
Johann Deneux <johann.deneux@gmail.com>
2020-07-21 20:42:17 +0200

    Add damages to static planes from results

--- Campaign/Src/MissionResults.fs
+++ Campaign/Src/MissionResults.fs
@@ -13,29 +13,46 @@ open Campaign.WarStateUpdate
 open Campaign.WorldDescription
 
 type IWarStateQuery with
+    /// Get the nearest airfield to a position
     member this.GetNearestAirfield((x, _, z) : Position) =
         let p = Vector2(x, z)
         this.World.Airfields.Values
         |> Seq.minBy (fun af -> (af.Position - p).Length())
 
-    member this.TryGetPlane(modelName : string) =
+    /// Try to get a plane by its name as it appears in the logs
+    member this.TryGetPlane(logName : string) =
         this.World.PlaneSet.Values
-        |> Seq.tryFind (fun plane -> modelName.Contains(plane.LogName))
+        |> Seq.tryFind (fun plane -> logName.Contains(plane.LogName))
 
-    member this.GetBuildingsAt(modelName : string, part : int, (x, _, z) : Position) =
-        let modelName = modelName.ToLowerInvariant()
+    /// Get all buildings that match a name, have the given subpart as reported as important, and cover the given position.
+    member this.GetBuildingsAt(logName : string, part : int, (x, _, z) : Position) =
+        let logName = logName.ToLowerInvariant()
         let pos = Vector2(x, z)
         Seq.append this.World.Buildings.Values this.World.Bridges.Values
         |> Seq.filter (fun building ->
-            building.Properties.Model.ToLowerInvariant().Contains(modelName) &&
+            building.Properties.Model.ToLowerInvariant().Contains(logName) &&
             List.contains part building.Properties.SubParts &&
             pos.IsInConvexPolygon building.Boundary)
 
+    /// Try to get the static plane at the given position, and the airfield it is assigned to.
+    member this.TryGetStaticPlaneAt(logName : string, ((x, _, z) as pos)) =
+        // We don't check that there actually is a plane at that position, only that's it's "close enough" to the nearest airfield.
+        let af = this.GetNearestAirfield(pos)
+        if (af.Position - Vector2(x, z)).Length() < 3000.0f then
+            let logName = logName.ToLowerInvariant()
+            this.World.PlaneSet.Values
+            |> Seq.tryFind (fun plane -> logName.Contains(plane.StaticBasename.ToLowerInvariant()))
+            |> Option.map (fun plane -> af.AirfieldId, plane)
+        else
+            None
+
+/// The status of a plane piloted by a human pilot.
 type FlightStatus =
     | Spawned of AirfieldId
     | InAir of TookOffFrom: AirfieldId
     | Landed of AirfieldId
 
+/// Extract war state updade commands from the game logs.
 let commandsFromLogs (state : IWarStateQuery) (logs : AsyncSeq<string>) =
     asyncSeq {
         let bindings = Seq.mutableDict []
@@ -85,13 +102,18 @@ let commandsFromLogs (state : IWarStateQuery) (logs : AsyncSeq<string>) =
                 let health = health - damaged.Damage
                 healthOf.[damaged.TargetId] <- health
                 // Emit DamageBuildingPart
+                // Emit RemovePlane for damages to static planes
                 match bindings.TryGetValue(damaged.TargetId) with
                 | true, binding ->
                     for building in state.GetBuildingsAt(binding.Name, binding.Sub, damaged.Position) do
                         yield DamageBuildingPart(building.Id, binding.Sub, damaged.Damage)
+                    match state.TryGetStaticPlaneAt(binding.Name, damaged.Position) with
+                    | Some(afId, plane) ->
+                        yield RemovePlane(afId, plane.Id, damaged.Damage)
+                    | None ->
+                        ()
                 | _ ->
                     ()
-
 
             | PlayerEvent(_, PlayerEndsMission missionEnded) ->
                 // Emit AddPlane command
@@ -109,4 +131,7 @@ let commandsFromLogs (state : IWarStateQuery) (logs : AsyncSeq<string>) =
                 vehicleOf.Remove(missionEnded.PilotId) |> ignore
                 flights.Remove(missionEnded.VehicleId) |> ignore
                 healthOf.Remove(missionEnded.VehicleId) |> ignore
+
+            | _ ->
+                ()
     }
