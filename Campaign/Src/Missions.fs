b35d8636b9fc47166ebb77148a314ae2e8f29139
Johann Deneux <johann.deneux@gmail.com>
2020-08-06 18:48:38 +0200

    Move target types into a separate file
    Purpose is to make it available to modules with lower dependencies

--- Campaign/Src/Missions.fs
+++ Campaign/Src/Missions.fs
@@ -26,63 +26,10 @@ open Campaign.BasicTypes
 open Campaign.PlaneModel
 open Campaign.WorldDescription
 open Campaign.NewWorldDescription
+open Campaign.Targets
 open Campaign.WarState
 open Campaign.WarStateUpdate
 open Util
-
-
-type TargetType =
-    | Truck | Train | Ship | Battleship | GunBoat | Artillery | Tank | ArmoredCar
-    | Bridge of BuildingInstanceId * int
-    | Building of BuildingInstanceId * int
-    | ParkedPlane of AirfieldId * PlaneModelId
-    | Air of PlaneModelId
-with
-    member this.GroundForceValue =
-        match this with
-        | Battleship -> 100.0f<MGF>
-        | GunBoat -> 15.0f<MGF>
-        | Artillery -> 10.0f<MGF>
-        | Tank -> 25.0f<MGF>
-        | ArmoredCar -> 5.0f<MGF>
-        | _ -> 0.0f<MGF>
-
-module TargetType =
-    let (|GroundForceTarget|_|) (kind : TargetType) =
-        let value = kind.GroundForceValue
-        if value > 0.0f<MGF> then
-            Some value
-        else None
-
-    /// Match the name of an object from the log with a target type.
-    // This is dependent on the MCUs in the mission and their naming.
-    let (|TargetTypeByName|_|) (name : string) =
-        match name with
-        | "CANNON" -> Some Artillery
-        | _ -> None
-
-type Target =
-    {
-        Kind : TargetType
-        Owner : CoalitionId option
-        Pos : OrientedPosition
-    }
-
-type AmmoType =
-    Rocket | Bullets | Bomb
-
-type ReturnType =
-    CrashedInEnemyTerritory | CrashedInFriendlyTerritory | AtAirfield of AirfieldId
-
-/// The results of a flight by a player, used to build success rates of missions.
-type FlightRecord =
-    {
-        Date : DateTime
-        Plane : PlaneModel
-        Start : AirfieldId
-        TargetsDestroyed : (Target * AmmoType) list
-        Return : ReturnType
-    }
 
 type AltitudeLevel = LowAltitude | MediumAltitude | HighAltitude
 with
