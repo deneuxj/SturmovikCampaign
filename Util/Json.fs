b62b67d0bc891a2f08bcac55e900a7b5a266d68b
Johann Deneux <johann.deneux@gmail.com>
2020-08-28 21:22:30 +0200

    Add Json transform for Vector2

--- Util/Json.fs
+++ Util/Json.fs
@@ -1,6 +1,7 @@
 ﻿namespace Util.Json
 
 open FSharp.Json
+open System.Numerics
 
 /// Add support for JSON (de)serialization of TimeSpan, using attribute FSharp.Json.JsonField(Transform = typeof<TimeSpanTransform>)
 type TimeSpanTransform() =
@@ -16,3 +17,25 @@ type TimeSpanTransform() =
             match arg1 with
             | :? System.TimeSpan as span -> box span.Ticks
             | _ -> failwith "Not a TimeSpan"
+
+type TimeSpanJsonField() =
+    inherit JsonField(Transform = typeof<TimeSpanTransform>)
+
+/// Add support for JSON (de)serialization of Vector2, using attribute FSharp.Json.JsonField(Transform = typeof<Vector2Transform>)
+type Vector2Transform() =
+    interface ITypeTransform with
+        member this.targetType(): System.Type =
+            typeof<float32 * float32>
+
+        member this.fromTargetType(arg1: obj): obj =
+            let xy : float32 * float32 = unbox arg1
+            let x, y = xy
+            Vector2(x, y) :> obj
+
+        member this.toTargetType(arg1: obj): obj =
+            match arg1 with
+            | :? Vector2 as v -> box(v.X, v.Y)
+            | _ -> failwith "Not a TimeSpan"
+
+type Vector2JsonField() =
+    inherit JsonField(Transform = typeof<Vector2Transform>)
