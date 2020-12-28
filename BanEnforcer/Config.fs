00be695cf6a39ebb4e0c4ed1732f45fe29293486
Johann Deneux <johann.deneux@gmail.com>
2020-12-28 12:54:54 +0100

    Add config type

---
+++ BanEnforcer/Config.fs
@@ -0,0 +1,27 @@
+﻿module Coconutside.BanEnforcer.Config
+
+open FSharp.Json
+
+type Config =
+    {
+        DServerRCons : {| Url : string; User : string; Password : string |} list
+        PlayerDbPath : string option
+        CheckInterval : int option
+        HtmlPath : string option
+    }
+with
+    /// The file that contains the player bans
+    member this.GetPlayerDbPath() =
+        let path =
+            this.PlayerDbPath
+            |> Option.defaultValue "playerdb.json"
+        if path.EndsWith "/" || path.EndsWith "\\" then
+            path + "playerdb.json"
+        else
+            path
+
+    /// The check interval, in seconds
+    member this.GetCheckInterval() =
+        this.CheckInterval
+        |> Option.defaultValue 15
+
